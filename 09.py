9baa8f4cd8d472bd03ffde0e908b283c970e06cd
Henrique <henriquemachado137@gmail.com>
2023-11-28 17:54:53 -0300

    commit

--- 09.py
+++ 09.py
@@ -2,39 +2,31 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/teste/1', methods=['GET' , 'POST'])
-def analise_numeros():
+# Importar a classe Flask e o objeto request:
+from flask import Flask, request, render_template
+
+# Criar o objeto Flask app:
+app = Flask(__name__)
+
+# http://127.0.0.1:5000/teste/1
+# Aceita requisições com os métodos GET e POST.
+# GET: gera um formulário em HTML para o usuário
+# enviar dados para o servidor.
+# POST: lê os dados enviados pelo usuário através
+# do formulário HTML.
+@app.route('/teste/1', methods=['GET', 'POST'])
+def teste_dados_formulario_html():
     # Trata a requisição com método POST:
     if request.method == 'POST':
-        # Obter os três números do formulário
-        num1 = float(request.form.get('num1'))
-        num2 = float(request.form.get('num2'))
-        num3 = float(request.form.get('num3'))
-
-        # Calcular o menor número
-        menor_numero = min(num1, num2, num3)
-
-        # Calcular o maior número
-        maior_numero = max(num1, num2, num3)
-
-        # Calcular a média dos números
-        media = (num1 + num2 + num3) / 3
-
-        # Retornar os resultados como HTML
-        return '''
-        <h1>Maior número: {}</h1>
-        <h1>Menor número: {}</h1>
-        <h1>Média: {}</h1>
-        '''.format(maior_numero, menor_numero, media)
-
-    return '''
-    <form method="POST" action="/teste/1">
-        <div><label>Número 1: <input type="text" name="num1"></label></div>
-        <div><label>Número 2: <input type="text" name="num2"></label></div>
-        <div><label>Número 3: <input type="text" name="num3"></label></div>
-        <input type="submit" value="Enviar">
-    </form>
-    '''
+        linguagem = request.form.get('linguagem')
+        framework = request.form.get('framework')
+        # Ou:
+        # linguagem = request.form['linguagem']
+        # framework = request.form['framework']
+        return render_template('resposta.html', linguagem_usr=linguagem,
+                               framework_usr=framework)
+    # Caso contrário, trata a requisição com método GET:
+    return render_template('index.html')
 
 if __name__ == '__main__':
     # Executar app no modo debug (default) na porta 5000 (default):
---
+++ 10.py
@@ -0,0 +1,33 @@
+# Importar a classe Flask e o objeto request:
+from flask import Flask, request
+
+# Criar o objeto Flask app:
+app = Flask(__name__)
+
+# http://127.0.0.1:5000/teste/1
+# Aceita requisições com o método POST.
+# O corpo da requisição deve conter um objeto JSON
+# como o apresentado abaixo:
+# {
+# "linguagem" : "Python",
+# "framework" : "Flask"
+# }
+@app.route('/teste/1', methods=['GET' , 'POST'])
+def teste_json():
+    objeto_json = request.get_json()
+
+    # Verificar se o objeto no formato JSON não é nulo.
+    if objeto_json is not None:
+        if 'linguagem' in objeto_json:
+            linguagem = objeto_json['linguagem']
+        if 'framework' in objeto_json:
+            framework = objeto_json['framework']
+
+        return '''
+        Linguagem informada: {}
+        Framework informado: {}
+        '''.format(linguagem, framework)
+
+if __name__ == '__main__':
+    # Executar app no modo debug (default) na porta 5000 (default):
+    app.run(debug=True, port=5000)
