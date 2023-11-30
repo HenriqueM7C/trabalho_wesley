5f562484ed3ff177724aa0d15548f86ae3166631
Henrique <henriquemachado137@gmail.com>
2023-11-30 15:02:40 -0300

    Commit

--- 10_1.py
+++ 10_1.py
@@ -1,14 +1,21 @@
+#Importa as classes necessárias do módulo Flask para criar uma aplicação web e lidar com solicitações HTTP.
 from flask import Flask, request, jsonify
 
+#Cria uma instância da aplicação Flask.
 app = Flask(__name__)
 
+#recebe três parâmetros (lados de um triângulo) e retorna True se os lados fornecidos podem formar um triângulo e False caso contrário
 def eh_triangulo(x, y, z):
     if (x + y > z) and (y + z > x) and (z + x > y):
         return True
     else:
         return False
 
+#Define uma rota chamada /teste/1 que aceita apenas solicitações HTTP POST.
 @app.route('/teste/1', methods=['POST'])
+#Função associada à rota. Recebe dados JSON contendo os lados do triângulo 
+#e retorna se esses lados podem formar um triângulo ou não.
+#A função tenta obter os dados JSON da solicitação usando request.get_json().
 def verificar_triangulo():
     try:
         dados_json = request.get_json()
